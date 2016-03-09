3d68f4f3c2bbbb261fa879c0c509a28f48dfe452
Jonathan Garvin <jon@5valleys.com>
2016-03-08 19:18:35 -0700

    add initial hivebot page

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,1 +1,162 @@
-/*! skinny-bones-jekyll - v1.0.0 - 2015-11-17 */!function($){"use strict";$.fn.fitVids=function(options){var settings={customSelector:null,ignore:null};if(!document.getElementById("fit-vids-style")){var head=document.head||document.getElementsByTagName("head")[0],css=".fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}",div=document.createElement("div");div.innerHTML='<p>x</p><style id="fit-vids-style">'+css+"</style>",head.appendChild(div.childNodes[1])}return options&&$.extend(settings,options),this.each(function(){var selectors=["iframe[src*='player.vimeo.com']","iframe[src*='youtube.com']","iframe[src*='youtube-nocookie.com']","iframe[src*='kickstarter.com'][src*='video.html']","object","embed"];settings.customSelector&&selectors.push(settings.customSelector);var ignoreList=".fitvidsignore";settings.ignore&&(ignoreList=ignoreList+", "+settings.ignore);var $allVideos=$(this).find(selectors.join(","));$allVideos=$allVideos.not("object object"),$allVideos=$allVideos.not(ignoreList),$allVideos.each(function(){var $this=$(this);if(!($this.parents(ignoreList).length>0||"embed"===this.tagName.toLowerCase()&&$this.parent("object").length||$this.parent(".fluid-width-video-wrapper").length)){$this.css("height")||$this.css("width")||!isNaN($this.attr("height"))&&!isNaN($this.attr("width"))||($this.attr("height",9),$this.attr("width",16));var height="object"===this.tagName.toLowerCase()||$this.attr("height")&&!isNaN(parseInt($this.attr("height"),10))?parseInt($this.attr("height"),10):$this.height(),width=isNaN(parseInt($this.attr("width"),10))?$this.width():parseInt($this.attr("width"),10),aspectRatio=height/width;if(!$this.attr("id")){var videoID="fitvid"+Math.floor(999999*Math.random());$this.attr("id",videoID)}$this.wrap('<div class="fluid-width-video-wrapper"></div>').parent(".fluid-width-video-wrapper").css("padding-top",100*aspectRatio+"%"),$this.removeAttr("height").removeAttr("width")}})})}}(window.jQuery||window.Zepto),function($){var $w=$(window);$.fn.visible=function(partial,hidden,direction){if(!(this.length<1)){var $t=this.length>1?this.eq(0):this,t=$t.get(0),vpWidth=$w.width(),vpHeight=$w.height(),direction=direction?direction:"both",clientSize=hidden===!0?t.offsetWidth*t.offsetHeight:!0;if("function"==typeof t.getBoundingClientRect){var rec=t.getBoundingClientRect(),tViz=rec.top>=0&&rec.top<vpHeight,bViz=rec.bottom>0&&rec.bottom<=vpHeight,lViz=rec.left>=0&&rec.left<vpWidth,rViz=rec.right>0&&rec.right<=vpWidth,vVisible=partial?tViz||bViz:tViz&&bViz,hVisible=partial?lViz||lViz:lViz&&rViz;if("both"===direction)return clientSize&&vVisible&&hVisible;if("vertical"===direction)return clientSize&&vVisible;if("horizontal"===direction)return clientSize&&hVisible}else{var viewTop=$w.scrollTop(),viewBottom=viewTop+vpHeight,viewLeft=$w.scrollLeft(),viewRight=viewLeft+vpWidth,offset=$t.offset(),_top=offset.top,_bottom=_top+$t.height(),_left=offset.left,_right=_left+$t.width(),compareTop=partial===!0?_bottom:_top,compareBottom=partial===!0?_top:_bottom,compareLeft=partial===!0?_right:_left,compareRight=partial===!0?_left:_right;if("both"===direction)return!!clientSize&&viewBottom>=compareBottom&&compareTop>=viewTop&&viewRight>=compareRight&&compareLeft>=viewLeft;if("vertical"===direction)return!!clientSize&&viewBottom>=compareBottom&&compareTop>=viewTop;if("horizontal"===direction)return!!clientSize&&viewRight>=compareRight&&compareLeft>=viewLeft}}}}(jQuery),$(document).ready(function(){$("#js-menu-trigger,#js-menu-screen").on("click touchstart",function(e){$("#js-menu, #js-menu-screen").toggleClass("is-visible"),$("#js-menu-trigger").toggleClass("slide close"),e.preventDefault()})}),$(document).ready(function(){$("#main").fitVids()}),$("#markdown-toc").prepend("<li><h6>Overview</h6></li>");
+var app = angular.module('chartApp', []);
+
+app.controller('TempChartController', [TempChartController]);
+function TempChartController() {
+  self = this;
+}
+
+app.directive('linearChart', ['$window', linearChartDirective]);
+function linearChartDirective($window) {
+  self = this;
+  var linesToPlot = [];
+  
+  self.link = function(scope, elem, attrs) {
+    self.scope = scope;
+    self.elem = elem;
+    self.attrs = attrs;
+    var columnsToChart = self.attrs.chartColumnNames.split(',')
+      .map(function(x) { return $.trim(x) });
+    var nestsToChart = self.attrs.chartNestNames.split(',')
+      .map(function(x) { return $.trim(x) });
+    var nestIdentifier = self.attrs.nestNamesColumnName
+    rawSvg().attr("width", width());
+    rawSvg().attr("height", height());
+
+    var scale = d3.scale.ordinal()
+
+    d3.csv(self.attrs.chartDataUrl, function(error, data) {
+      dataNest = d3.nest()
+        .key(function(d) { return d[nestIdentifier] })
+        .entries(data)
+
+      dataColumns = d3.keys(data[0]).filter(function(key) {
+        return ($.inArray(key, columnsToChart) > -1)
+      })
+
+      nestedColumns = []
+      dataNest.forEach(function(nest) {
+        if ($.inArray(nest.key, nestsToChart) > -1) {
+          dataColumns.forEach(function(column) {
+            nestedColumns.push([nest.key, column]);
+          })  
+        }
+      })
+      scale.domain(nestedColumns);
+
+      linesToPlot = scale.domain().map(function(key, index) {
+        filtered_data = data.filter(function(d) {
+          return d[nestIdentifier] == key[0]
+        });
+        return { 
+          counter: index+1,
+          name: key.join('_'),
+          values: filtered_data.map(function(d) {
+            var newd =  { x: parseDate(d.timestamp), y: +d[key[1]] };
+            return newd;
+          })
+        };
+      });
+
+      svg().append("svg:g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0,"+(height() - padding())+")")
+        .call(xAxisGen());
+        
+      svg().append("svg:g")
+        .attr("class", "y axis")
+        .attr("transform", "translate("+padding()+",0)")
+        .call(yAxisGen());
+        
+       var lineSet = svg().selectAll(".line")
+         .data(linesToPlot)
+         .enter().append("g")
+         .attr("class", "line");
+
+       lineSet.append("svg:path")
+         .attr("class", function(d) {
+           return ['line',  d.name, ordclass(d.counter)].join(' ');
+         })
+         .attr("d", function(d) { return dataLine()(d.values); })
+         .style("stroke", function(d) { return scale(d.name); });
+
+    });
+  }
+  
+  var parseDate = d3.time.format.utc("%Y-%m-%d %H:%M:%S UTC").parse;
+
+  var ordclass = function(n) {
+    return 'ord' + ordinal(n)
+  }
+
+  var ordinal = function(n) {
+    var s=["th","st","nd","rd"],
+        v=n%100;
+    return n+(s[(v-20)%10]||s[v]||s[0]);
+  }
+
+  var dataLine = function() {
+    return d3.svg.line()
+             .x(function (d) { return xScale()(d.x); })
+             .y(function (d) { return yScale()(d.y); })
+             .interpolate("basis");
+  }
+  
+  var yAxisGen = function() {
+    return d3.svg.axis()
+             .scale(yScale())
+             .orient("left")
+             .ticks(5);    
+  }
+  
+   var xAxisGen = function() {
+    return d3.svg.axis()
+             .scale(xScale())
+             .orient("bottom")
+             .ticks(8);
+  }
+  
+  var yScale = function() {
+    maxY = d3.max(linesToPlot, function (line) {
+      return d3.max(line.values, function(d) {
+        return d.y*1.1;
+      })
+    })
+    return d3.scale.linear()
+             .domain([0, maxY])
+             .range([height() - padding(), 0]);
+  }
+  
+   var xScale = function() {
+    minX = d3.min(linesToPlot, function(line) { return line.values[0].x });
+    maxX = d3.max(linesToPlot, function(line) { return line.values[line.values.length-1].x });
+    return d3.time.scale()
+      .domain([minX, maxX])
+      .range([0 + padding(), width()]);
+  }
+  
+   var svg = function() {
+    return d3.select(rawSvg()[0]);
+  }
+  
+  var rawSvg = function() {
+    return self.elem.find('svg');
+  }
+  
+  var height = function() {
+    return self.elem[0].clientHeight
+  }
+  
+  var width = function() {
+    return self.elem[0].clientWidth;
+  }
+    
+  var padding = function() {
+    return eval(self.attrs.padding);
+  }
+  
+  return {
+    restrict:'EA',
+    template:"<svg></svg>",
+    link: link
+  };
+};
